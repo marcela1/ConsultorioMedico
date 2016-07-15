b8ac5250a887f7d4270a23ffd959a1d7d236ac91
marcela1 <marcelacarrillo88@gmail.com>
2016-07-15 11:54:57 -0600

    barra de navegacion fija

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -21,7 +21,7 @@
 </head>
 
 
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-inverse navbar-fixed-top" >
   <div class="container-fluid">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-2">
