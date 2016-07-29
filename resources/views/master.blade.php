1b7a1d77eaf9ce5c36e290aabf9f80fe9bf7e7d0
Jesus Aispuro <dany_aispuro@hotmail.com>
2016-07-28 23:45:22 -0600

    login2

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -63,10 +63,23 @@
         </div>
         <button type="submit" class="btn btn-default">Buscar</button>
       </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="{{url('/login')}}">Login</a></li>
-         <li><a href="{{url('/register')}}">Registrarse</a></li>
-      </ul>
+     <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
     </div>
   </div>
 </nav>
